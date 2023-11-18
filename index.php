552302b0b97aca81d4b7d35241657e0549f99643
IsaacDp <isaacdp09@gmail.com>
2023-11-17 20:43:00 -0600

    Header de la pagina web sin estilizar

--- index.php
+++ index.php
@@ -6,7 +6,54 @@
     <title>Tortas el rinconcito</title>
 </head>
 <body>
-    <h1>Hola mundo</h1>
-    <p>Prueba inicial del index.php</p>
+    <header>
+        <div class="container-hero">
+            <div class="container hero">
+                <div class="customer-support">
+                    <i class="fa-solid fa-phone"></i>
+                    <div class="content-customer-support">
+                        <span class="text">Comunícate con nosotros</span>
+                        <span ckass="number">55 81 39 96 80</span>
+                    </div>
+                </div>
+                <div class="container-logo">
+                    <i class="fa-solid fa-burger"></i>
+                    <h1 class="logo">
+                        <a href="/">Tortas el Rinconcito</a>
+                    </h1>
+                </div>
+                <div class="container-user">
+                    <i class="fa-solid fa-user"></i>
+                    <i class="fa-solid fa-money-bill"></i>
+                    <div class="content-ticket">
+                        <span class="text">Cupones</span>
+                        <span class="number">(0)</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-navbar">
+            <nav class="navbar conainer">
+                <i class="fa-solid fa-bars"></i>
+                <ul class="menu">
+                    <li><a href="#">Tortas</a></li>
+                    <li><a href="#">Desayunos</a></li>
+                    <li><a href="#">Bebidas</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Ligas de interés</a></li>
+                </ul>
+
+                <form class="search-form">
+                    <input type="text" placeholder="Buscar...">
+                    <button class="btn-search">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </form>
+            </nav>
+        </div>
+    </header>
+
+    <script src="https://kit.fontawesome.com/81581fb069.js"
+    crossorigin="anonymous"></script>
 </body>
 </html>
