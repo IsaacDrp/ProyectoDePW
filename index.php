5319aa9e114727d3c3de565c43c46d16a9181011
IsaacDp <isaacdp09@gmail.com>
2023-11-18 15:49:26 -0600

    creación y estilización de una galeria simple

--- index.php
+++ index.php
@@ -188,6 +188,50 @@
                 </div>
             </div>
         </section>
+        
+        <style>
+            .gallery {
+	        display: grid;
+	        grid-template-columns: repeat(4, 1fr);
+            grid-template-rows: repeat(2, 30rem);
+            gap: 1.5rem;
+            margin-bottom: 3rem;
+        }
+        .gallery img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .gallery-img-3 {
+            grid-column: 2/4;
+            grid-row: 1/3;
+        }
+        </style>
+
+        <section class="gallery">
+				<img
+					src="media/gallery/3.jpg"
+					alt="Gallery Img1"
+					class="gallery-img-1"
+				/><img
+					src="media/gallery/2.jpg"
+					alt="Gallery Img2"
+					class="gallery-img-2"
+				/><img
+					src="media/gallery/1.jpg"
+					alt="Gallery Img3"
+					class="gallery-img-3"
+				/><img
+					src="media/gallery/4.jpg"
+					alt="Gallery Img4"
+					class="gallery-img-4"
+				/><img
+					src="media/gallery/5.jpg"
+					alt="Gallery Img5"
+					class="gallery-img-5"
+				/>
+			</section>
     </main>
 
     <script src="https://kit.fontawesome.com/81581fb069.js"
