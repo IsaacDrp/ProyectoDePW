af48829b970b87cea55784fbb2bf562d3c6c0855
IsaacDp <isaacdp09@gmail.com>
2023-11-19 18:16:32 -0600

    creacion del archivo de conexion a la BD

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+    $host_db="localhost:3306";
+    $user_db="root";
+    $pass_db="root";
+    $db_name="TORTERIA";
+
+    $conexion = new mysqli($host_db,$user_db,$pass_db,$db_name);
+
+    if($conexion->connect_error){
+        echo"<h1>MySQL no te está dando permisos para ejecutar las consultas</h1>";
+    } else {
+        echo"<h2 style='color: greenyellow; text-align: center;'>Hola, tu conexión está funcionando.</h2>";
+    }
+
+?>
